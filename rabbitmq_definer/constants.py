f67088085533d733141bdde330d14220717a24d8
danicassani <danielduran1995@gmail.com>
2024-06-09 23:25:14 +0200

    added the view functionality and placeholder for the rest of the actions

--- rabbitmq_definer/constants.py
+++ rabbitmq_definer/constants.py
@@ -5,4 +5,12 @@ LEVEL_CHOICES = (
     ("PB", "Público")
 )
 
+SCHEMA_DEFINITIONS_ACTION = "schema_definitions"
+FEDERATIONS_ACTION = "federations"
+POLICIES_ACTION = "policies"
+SHOVELS_ACTION = "shovels"
+
 SCHEMA_DEFINITIONS_PATH = "schema.definitions.json"
+FEDERATIONS_PATH = "Federations.json"
+POLICIES_PATH = "Policies.json"
+SHOVELS_PATH = "Shovels.json"
--- rabbitmq_definer/views.py
+++ rabbitmq_definer/views.py
@@ -1,42 +1,68 @@
 from django.shortcuts import render
-from .dict_getters import get_full_dict
-from django.http import HttpRequest, HttpResponse
+from .dict_getters import get_full_schema_definitions, get_full_federations
+from django.http import HttpRequest, FileResponse
+from django.http.response import HttpResponseBadRequest
 from .models import Schema
-from django.http import FileResponse
 
-from .constants import SCHEMA_DEFINITIONS_PATH
-from django.core.files.temp import NamedTemporaryFile
-from wsgiref.util import FileWrapper
-
+from .constants import *
 import json, os
 
 # Create your views here.
 
 def home(request: HttpRequest):
     if request.method == 'POST':
-        # action = request.POST.get('action')
+        action = request.POST.get('action')
         schema_pk = request.POST.get('schema')
         schema = Schema.objects.get(pk=schema_pk) if schema_pk else None
-        try:
-            full_dict = get_full_dict(schema)
-
+        file_path = ""
+        
+        if action == SCHEMA_DEFINITIONS_ACTION:
             file_path = SCHEMA_DEFINITIONS_PATH
-
+            try:
+                full_dict = get_full_schema_definitions(schema)
+            except Exception as e:
+                print(f"Exception getting schema definitions: {e}")
+        
+        elif action == FEDERATIONS_ACTION:
+            file_path = FEDERATIONS_PATH
+            try:
+                full_dict = get_full_federations(schema)
+            except Exception as e:
+                print(f"Exception getting federations: {e}")
+            
+        elif action == POLICIES_ACTION:
+            file_path = POLICIES_PATH
+            try:
+                pass
+            except Exception as e:
+                print(f"Exception getting policies: {e}")
+            
+        elif action == SHOVELS_ACTION:
+            file_path = SHOVELS_PATH
+            try:
+                pass
+            except Exception as e:
+                print(f"Exception getting shovels: {e}")
+        
+        else:
+            return HttpResponseBadRequest()
+            
+        try:
             # Write the dictionary to a JSON file
             with open(file_path, 'w') as json_file:
                 json.dump(full_dict, json_file, indent=4)
 
             response = FileResponse(open(file_path, 'rb'), content_type='application/json')
-            response['Content-Disposition'] = f'attachment; filename="{file_path}"'
-            
-            # Remove the file after serving
-            os.remove(file_path)
-            
-            return response
 
         except Exception as e:
             print(f"Exception getting full dict: {e}")
         
+        response['Content-Disposition'] = f'attachment; filename="{file_path}"'
+        
+        # Remove the file after serving
+        os.remove(file_path)
+        
+        return response
     
     # else and except refresh the view
     schemas = Schema.objects.all()
